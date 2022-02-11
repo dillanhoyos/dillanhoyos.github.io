414c96693ab5c9dd74fb43ad5f43fe7c37f94dac
dillanhoyos <dillanhoyosmusic@gmail.com>
2022-02-11 18:42:06 -0500

    here
    here

--- docs/contact.php
+++ docs/contact.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <demo@domain.com>'; // Add Your Email
+$sendTo = 'Demo contact form <dillanhoyosmusic@gmail.com>'; // Add Your Email
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
