6348b161ba4b271528c4be6444fbf74ea076aebb
AudioWall <dillanhoyosmusic@gmail.com>
2023-02-10 13:01:24 -0500

    New site

--- docs/js/Navbar.js
+++ docs/js/Navbar.js
@@ -7,11 +7,10 @@ function generateNavbar() {
                 <!-- Place your logo here -->
             </div>
                 <li><a href="index.html">About me</a></li>
-                <li><a href="VideoGames.html">VideoGames</a></li>
+                <li><a href="DreamMachine.html">VideoGames</a></li>
                 <li><a href="SoundDesign.html">Sound Design</a></li>
-                <li><a href="page4.html">Audio Programming</a></li>
-                <li><a href="page5.html">Music</a></li>
-                <li><a href="page6.html">Item 6</a></li>
+                <li><a href="Music.html">Music</a></li>
+           
         </ul>
     </nav>
     `;
